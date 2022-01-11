e1960e6c4c43f6bf5a2421e7d2a30945c8ba68f7
numbing <mirnumbing@gmail.com>
2022-01-12 00:20:34 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+        question: "in german ..... means hello",
+        answer: "hallo",
+        image: 'images/deutsche.jpeg'
+    },
+
+    {
+        question: "Germany Has ....... Varieties of Sausages!",
+        answer: "1000",
+        image: "images/sausages.jpeg"
+
+    }, {
+        question: "The Most Popular German Surname is .....",
+        answer: "muller",
+        image: 'images/typical.jpeg'
+
+    },
+    {
+        question: "The First Oktoberfest Was a ......",
+        answer: "wedding",
+        image: 'images/october.jpeg'
+
+    },
+
+]
+
---
+++ index.js
@@ -0,0 +1,33 @@
+let container = document.querySelector('#container');
+let myAnswer = ''
+
+
+for (let i = 0; i < data.length; i++) {
+    let myDiv = document.createElement("div");
+
+    myDiv.innerHTML = `
+        <div class="card" >
+            <img src="${data[i].image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">question ${i + 1}</h5>
+                <p class="card-text">${data[i].question}</p>
+                <input class="answerInput"/>
+                <button onclick="getInput(event,'${data[i].answer}')" class="btn btn-primary">Answer the question!</button>
+                <p class="result"> <p/>
+            </div>
+        </div>
+    `
+    container.appendChild(myDiv);
+
+}
+
+function getInput(event, answer) {
+    myAnswer = event.target.parentElement.querySelector('.answerInput').value
+
+    if (myAnswer === answer) {
+        myAnswer = event.target.parentElement.querySelector('.result').innerHTML = "niceeee"
+    } else {
+        myAnswer = event.target.parentElement.querySelector('.result').innerHTML = "wrong"
+    }
+
+}
